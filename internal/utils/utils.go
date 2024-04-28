4e99c3bd78ac6d7c722749469bb3c61da222c279
LucasNT <lucasntakahashi@gmail.com>
2024-04-28 06:42:53 -0300

    fix: output ask of the password outside of the stdout

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,13 +2,14 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"syscall"
 
 	"golang.org/x/term"
 )
 
 func ReadPassword() (string, error) {
-	fmt.Println("Password: ")
+	fmt.Fprintln(os.Stderr, "Password: ")
 	bytePwd, err := term.ReadPassword(int(syscall.Stdin))
 	if err != nil {
 		return "", fmt.Errorf("Failed to read password, %w", err)
